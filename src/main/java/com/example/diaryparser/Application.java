ad2a1dda394913750fe10a6de26440a23c5833b0
Owen <64725743+Ownwn@users.noreply.github.com>
2025-05-14 21:30:26 +1200

    Proper search filtering

--- src/main/java/com/example/diaryparser/Application.java
+++ src/main/java/com/example/diaryparser/Application.java
@@ -9,6 +9,7 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -18,60 +19,42 @@ import java.util.regex.PatternSyntaxException;
 public class Application extends javafx.application.Application {
 
     private static TableView<Note> notesTable;
+    private static List<Note> allNotes = new ArrayList<>();
+    private static Set<String> colNames = new HashSet<>();
 
     public static void main(String[] args) {
         launch();
     }
 
-
-
-    @Override
-    public void start(Stage stage) {
-        notesTable = new TableView<>();
-
-
-
-        ObservableList<String> fileNames = FXCollections.observableArrayList();
-        ListView<String> items = new ListView<>(fileNames);
-
-
-
-        Button loadFiles = getLoadFilesButton(stage);
-
-
-
-        VBox root = new VBox(10);
-        root.getChildren().addAll(loadFiles, items, notesTable, getSearchBox());
-
-        Scene scene = new Scene(root, 600, 240);
-
-        stage.setTitle("Diary Parser");
-        stage.setScene(scene);
-        stage.setWidth(1000);
-        stage.setHeight(700);
-        stage.show();
-    }
-
     private static TextField getSearchBox() {
         TextField searchBox = new TextField();
         searchBox.setPromptText("Regex filter...");
-
+        searchBox.setPrefHeight(150);
 
 
         searchBox.setOnKeyTyped(_ -> {
             String text = searchBox.getText();
-            if (text == null) {
+            if (text == null || text.isBlank()) {
+                notesTable.setItems(FXCollections.observableArrayList(allNotes));
                 return;
             }
             Pattern p;
             try {
-                p = Pattern.compile(text);
+                p = Pattern.compile(text.toLowerCase());
+                searchBox.setStyle("-fx-text-fill: black;");
             } catch (PatternSyntaxException e) {
-                searchBox.setText("Failed to compile regex! " + e.getDescription());
+                searchBox.setStyle("-fx-text-fill: red;");
                 return;
             }
 
-            // todo remove empty elements
+            List<Note> filteredNotes = allNotes.stream()
+                    .filter(note -> colNames.stream()
+                            .map(name -> String.join("\n", note.entries().getOrDefault(name, List.of())))
+                            .anyMatch(content -> p.matcher(content.toLowerCase()).find()))
+                    .toList();
+
+
+            notesTable.setItems(FXCollections.observableList(filteredNotes));
 
             notesTable.getColumns().forEach(column -> {
                 // There must be a better way than cast trickery right?
@@ -79,19 +62,12 @@ public class Application extends javafx.application.Application {
                 typedColumn.setCellValueFactory(cellData -> {
                     Note note = cellData.getValue();
                     String content = String.join("\n", note.entries().getOrDefault(typedColumn.getText(), List.of()));
-                    if (!p.matcher(content).find()) {
-                        return new SimpleStringProperty("");
-                    }
+
                     return new SimpleStringProperty(content);
                 });
             });
+
             notesTable.refresh();
-
-
-
-
-
-
         });
 
         return searchBox;
@@ -100,13 +76,13 @@ public class Application extends javafx.application.Application {
     private static Button getLoadFilesButton(Stage stage) {
         Button loadFiles = new Button("Load files");
 
-        Set<String> colNames = new HashSet<>();
+        colNames = new HashSet<>();
 
         loadFiles.setOnAction(_ -> {
             List<File> files = DiaryParser.loadFiles(stage);
             notesTable.getColumns().clear();
 
-            var allNotes = DiaryParser.getNotes(files).<Note>mapMulti((note, c) -> {
+            allNotes = DiaryParser.getNotes(files).<Note>mapMulti((note, c) -> {
                 colNames.addAll(note.entries().keySet());
                 c.accept(note);
             }).toList();
@@ -128,5 +104,30 @@ public class Application extends javafx.application.Application {
 
         });
         return loadFiles;
+    }
+
+    @Override
+    public void start(Stage stage) {
+        notesTable = new TableView<>();
+
+
+        ObservableList<String> fileNames = FXCollections.observableArrayList();
+        ListView<String> items = new ListView<>(fileNames);
+
+
+        Button loadFiles = getLoadFilesButton(stage);
+        TextField searchBox = getSearchBox();
+
+
+        VBox root = new VBox(10);
+        root.getChildren().addAll(loadFiles, items, notesTable, searchBox);
+
+        Scene scene = new Scene(root, 600, 240);
+
+        stage.setTitle("Diary Parser");
+        stage.setScene(scene);
+        stage.setWidth(1000);
+        stage.setHeight(700);
+        stage.show();
     }
 }
